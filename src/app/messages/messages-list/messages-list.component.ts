7650f059c4fc0f062e6498d2cb8164913a8b44e5
ArianJafari <arian.jafari2233@gmail.com>
2026-01-05 10:10:54 +0330

    Using async pipe instead of ngOnInit and ...

--- src/app/messages/messages-list/messages-list.component.ts
+++ src/app/messages/messages-list/messages-list.component.ts
@@ -1,24 +1,21 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, input, OnInit } from '@angular/core';
 import { MessagesService } from '../messages.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-messages-list',
   standalone: true,
+  imports: [AsyncPipe],
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MessagesListComponent implements OnInit{
+export class MessagesListComponent {
   private messagesService = inject(MessagesService);
-  private cdRef = inject(ChangeDetectorRef)
+  messages$ = this.messagesService.messages$;
   messages: string[] = []
 
-  ngOnInit(){
-    this.messagesService.messages$.subscribe((messages) => {
-      this.messages = messages;
-      this.cdRef.markForCheck();
-    })
-  }
+  
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
